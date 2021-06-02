500a89b16402296520fa2f2b173161f885db90f2
brokensm1le <61251949+brokensm1le@users.noreply.github.com>
2021-06-02 20:00:27 +0300

    Add files via upload

---
+++ destroy_script.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_script : MonoBehaviour
+{
+    public void close_prefab(GameObject what) {
+        Destroy(what);
+    }
+}
